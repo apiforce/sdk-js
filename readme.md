b0591c2eaeb36d316e12df28dd4947c3337b79f6
bruno.goncalves <bugotech@gmail.com>
2022-09-30 15:51:01 -0300

    inicio

---
+++ readme.md
@@ -0,0 +1,14 @@
+# ApiForce - SDK JS
+SDK js para utilização da API da api-force.com
+
+## Instalação
+
+Para instalar o sdk, siga abaixo as instruções:
+
+```bash
+$ npm install apiforce-sdk --save # para instalar e adicionar no pacote package.json
+```
+
+## Licença
+
+[MIT](http://opensource.org/licenses/MIT)
---
+++ src/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    /**
+     * Envs
+     */
+    envProduction : 'production',
+    envSandbox    : 'sandbox',
+    envLocal      : 'local',
+
+    /**
+     * Endpoints
+     */
+    endpoints: {
+        production : 'https://api-force.com',
+        sandbox    : 'https://sandbox.api-force.com',
+        local      : 'http://localhost:8090',
+    }
+};
